12a489c07f6a3cf620994ef25a5f5ed3f1c61d40
shivangi-djangodev <shivangirawat0020@gmail.com>
2022-01-11 13:15:17 +0530

    created a nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-import Home from './Components/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Shivangi Rawat</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/about">About</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/gallery">Gallery</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     </div>
   );
 }
